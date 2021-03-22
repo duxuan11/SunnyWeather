4fb5e552f335b7016334d83c2948bd3e0831f654
534459072@qq.com <534459072@qq.com>
2021-03-22 18:21:56 +0800

    four commit

--- app/src/main/java/com/sunnyweather/android/logic/Repository.kt
+++ app/src/main/java/com/sunnyweather/android/logic/Repository.kt
@@ -60,4 +60,14 @@ object Repository {
         }
         emit(result)
     }
+
+    private fun<T> fire(context: CoroutineContext,block: suspend () -> Result<T>) =
+            liveData<Result<T>>(context) {
+                val result = try {
+                    block()
+                } catch (e: Exception) {
+                    Result.failure<T>(e)
+                }
+                emit(result)
+            }
 }
